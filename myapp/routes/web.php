1392a8c3d8a6918a4d99f3fc0cb079f880ff4e25
seito-developer <seito.horiguchi@gmail.com>
2023-04-04 17:54:06 +0900

    completed calling the API

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/get_pokemon_data', function (Request $request) {
+    $pokemon_name = $request->input('pokemon_name');
+    $response = Http::get("https://pokeapi.co/api/v2/pokemon/$pokemon_name");
+    $pokemon_data = $response->json();
+    // do something with pokemon data
+
+    return view('welcome', ['pokemon_data' => $pokemon_data]);
+})->name('get_pokemon_data');
